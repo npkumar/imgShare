e3277dc4c92d4975edda371c2ae74c30da77dfe2
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2016-01-04 17:04:51 +0800

    more viewModels to test render

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,36 @@
+var viewModel = {
+    image: {
+        uniqueId:       1,
+        title:          'Sample Image 1',
+        description:    'This is a sample.',
+        filename:       'sample1.jpg',
+        views:          0,
+        likes:          0,
+        timestamp:      Date.now()
+    },
+    comments: [
+        {
+            image_id:   1,
+            email:      'test@testing.com',
+            name:       'Test Tester',
+            gravatar:   'http://lorempixel.com/75/75/animals/1',
+            comment:    'This is a test comment...',
+            timestamp:  Date.now()
+        },{
+            image_id:   1,
+            email:      'test@testing.com',
+            name:       'Test Tester',
+            gravatar:   'http://lorempixel.com/75/75/animals/2',
+            comment:    'Another followup comment!',
+            timestamp:  Date.now()
+        }
+    ]
+};
+
+
 module.exports = {
     index: function(req, res) {
-        res.render('image');
+        res.render('image', viewModel);
     },
     create: function(req, res) {
         res.send('The image:create POST controller');
