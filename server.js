60acedf3e4bc445644a175eac95038d955c07e55
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2016-01-04 13:05:17 +0800

    configure module for app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-// config = require('./server/configure'),
+config = require('./server/configure'),
 app = express();
 
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
-// app = config(app);
+app = config(app);
 
 app.get('/', function(req, res){
    res.send('Hello World'); 
