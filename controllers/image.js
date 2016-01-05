7c97b8b55ea97f2b090b52115b98148facfb4854
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2016-01-05 16:44:19 +0800

    display image and comment on image view

--- controllers/image.js
+++ controllers/image.js
@@ -1,44 +1,43 @@
 var fs = require('fs'),
     path = require('path');
 
-var sidebar = require('../helpers/sidebar');
+var sidebar = require('../helpers/sidebar'),
+    Models = require('../models');;
 
 var viewModel = {
-    image: {
-        uniqueId:       1,
-        title:          'Sample Image 1',
-        description:    'This is a sample.',
-        filename:       'sample1.jpg',
-        views:          0,
-        likes:          0,
-        timestamp:      Date.now()
-    },
-    comments: [
-        {
-            image_id:   1,
-            email:      'test@testing.com',
-            name:       'Test Tester',
-            gravatar:   'http://lorempixel.com/75/75/animals/1',
-            comment:    'This is a test comment...',
-            timestamp:  Date.now()
-        },{
-            image_id:   1,
-            email:      'test@testing.com',
-            name:       'Test Tester',
-            gravatar:   'http://lorempixel.com/75/75/animals/2',
-            comment:    'Another followup comment!',
-            timestamp:  Date.now()
-        }
-    ]
+    image: {},
+    comments: []
 };
 
 
 module.exports = {
     index: function(req, res) {
-        sidebar(viewModel, function(viewModel){
-            res.render('image', viewModel); 
+        Models.Image.findOne({ filename: { $regex: req.params.image_id } }, function(err, image) {
+            if (err) { 
+                throw err; 
+            }
+            if (image) {
+                image.views = image.views + 1;
+                viewModel.image = image;
+                image.save();
+                
+                Models.Comment.find({ image_id: image._id}, {}, { sort: { 'timestamp': 1 }}, function(err, comments){
+                    if (err) { 
+                        throw err; 
+                    }
+
+                    viewModel.comments = comments;
+                    sidebar(viewModel, function(viewModel) {
+                        res.render('image', viewModel);
+                    });     
+                });  
+                
+            } else {
+                res.redirect('/');
+            } 
         });
     },
+    
     create: function(req, res) {
         var saveImage = function() {
             var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
