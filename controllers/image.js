9aeacf0d7ebf9169f249fbd3a3ec4fd55379f268
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2016-01-07 15:35:26 +0800

    save comment and reference to image id

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,8 @@ var fs = require('fs'),
     path = require('path');
 
 var sidebar = require('../helpers/sidebar'),
-    Models = require('../models');;
+    Models = require('../models'),
+    md5 = require('MD5');
 
 var viewModel = {
     image: {},
@@ -99,6 +100,21 @@ module.exports = {
         });
     },
     comment: function(req, res) {
-        res.send('The image:comment POST controller');
+        Models.Image.findOne({ filename: { $regex: req.params.image_id } }, function(err, image) {
+            if (!err && image) {
+                    var newComment = new Models.Comment(req.body);
+                    newComment.gravatar = md5(newComment.email);
+                    newComment.image_id = image._id;
+                    newComment.save(function(err, comment) {
+                    if (err) { 
+                        throw err; 
+                    }
+                        res.redirect('/images/' + image.uniqueId + '#' + comment._id);
+                    });
+                } else {
+                    res.redirect('/');
+                }
+        });
+        
     }
 };
